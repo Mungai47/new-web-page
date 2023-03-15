56f84559a19a9b675d0af93e8bf919e0d02a1b9e
Joseph_Mungai <josephmburumungai99@gmail.com>
2023-03-15 03:32:12 +0300

    Updated Javascript

---
+++ js/myScript.js
@@ -0,0 +1,22 @@
+//Javascript variables
+//Javascript const
+// Always declare variabe with the keyworrd const
+// const is a variable that cannot be changed
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+
+
+// Declaring a variable and assigning a value
+
+var carName = "Volvo";
+let carName = "Volvo";
+
+// one statement many variables
+var person = "John Doe", carName = "Volvo", price = 200;
+// or
+
+let person = "John Doe",
+    carName = "Volvo",
+    price = 200,
+    age = 50;
